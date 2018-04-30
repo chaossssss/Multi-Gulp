ed32da9548461b8ba869202ac3d4d4c2726b3fa2
ssc <1033729276@qq.com>
2018-04-30 17:31:25 +0800

    modify

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Multi-Gulp
 FXXX Project
 
+### Todo
 
-|目标|状态|
-|:-:|:-:|
-|ejs转换成html|×|
-|css添加前缀|√|
-|图片压缩|×|
-|css sprite|×|
-|hash|×|
+- [ ] 图片压缩
+- [ ] css sprite
+- [x] 模板页面
+- [x] ejs转换成html
+- [x] css添加前缀
+- [x] html格式化
--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,36 @@ var config = require('./config/config.js');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
+var htmlbeautifyOptions = {
+	indent_size: 4,
+	indent_char: ' ',
+	//可以让一个标签独占一行
+	unformatted: true,
+	// 默认情况下，body | head 标签前会有一行空格
+	extra_liners: []
+};
+
 gulp.task('createPC',function(){
 	config.pages.forEach(function(page){
-		gulp.src('config/template/pc_template.html')
-			.pipe($.rename(page + '.html'))
-			.pipe(gulp.dest(config.rootPath + 'views'))
+		gulp.src('config/template/pc_template.ejs')
+			.pipe($.rename(page + '.ejs'))
+			.pipe(gulp.dest(config.rootPath + 'ejs'))
 	})
 	gulp.src('config/template/header.ejs')
-		.pipe(gulp.dest(config.rootPath + 'views'))
+		.pipe(gulp.dest(config.rootPath + 'ejs'))
 	gulp.src('config/template/footer.ejs')
-		.pipe(gulp.dest(config.rootPath + 'views'))
+		.pipe(gulp.dest(config.rootPath + 'ejs'))
 })
 
 gulp.task('libjs',function() {
 	gulp.src('src/lib/*.js')
+})
+
+gulp.task('ejs',function(){
+	gulp.src('src/ejs/*.ejs')
+		.pipe($.ejs({},{}, { ext: '.html'}))
+		.pipe($.htmlBeautify(htmlbeautifyOptions))
+		.pipe(gulp.dest('src'))
 })
 
 gulp.task('js',function(){
@@ -42,14 +58,15 @@ gulp.task('css',function(){
 	gulp.src('src/css/*.css')
 })
 
-gulp.task('serve',['libjs','less','css','js'], function() {
+gulp.task('serve',['libjs','ejs','less','css','js'], function() {
     browserSync.init({
         server: "src",
     });
     gulp.watch("src/lib/*.js",['libjs']);
+    gulp.watch("src/ejs/*.ejs",['ejs']).on('change',reload);
     gulp.watch("src/less/*.less",['less']);
     gulp.watch("src/css/*.css",['css']).on('change', reload);
     gulp.watch("src/js/*.js",['js']);
-    gulp.watch("src/views/*.html").on('change', reload);
+    gulp.watch("src/*.html").on('change', reload);
 });
 gulp.task('default', ['serve']);
